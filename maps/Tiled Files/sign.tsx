7cbb6f80db965e84067e67c633ea3eac3a606297
dylsingh81 <dylan.singh@stonybrook.edu>
2021-04-23 13:55:00 -0400

    Revert "Sign updates"
    This reverts commit a3cb9d79e5a4a04e27c61ae8700497c96ecf5dda.

--- maps/Tiled Files/sign.tsx
+++ maps/Tiled Files/sign.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="sign" tilewidth="16" tileheight="16" tilecount="10" columns="5">
- <image source="sign_no_border.png" width="82" height="32"/>
+ <image source="sign.png" width="82" height="32"/>
 </tileset>
