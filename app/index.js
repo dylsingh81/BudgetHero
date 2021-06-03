8d6d1916cbe8eeb8f3763d59402c935d3f821fd9
dylsingh81 <dylan.singh@stonybrook.edu>
2021-06-03 19:51:27 -0400

    Databases fixes

--- app/index.js
+++ app/index.js
@@ -7,8 +7,10 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
-database.loadDatabase();
-
+database.loadDatabase(function (err) {    // Callback is optional
+   console.log("Database loaded")
+   //console.log(database.getAllData())
+});
 
 /*
 app.get('/api', (request, response) => {
@@ -28,17 +30,22 @@ app.post('/ip', (request, response) => {
     //Check if IP Exists
     var ip = data.ip
     database.find({ip}, (err, data_query) => {
-        console.log(data_query)
         //IP Exists Update Number of time Played
-        if(data_query.length != 0){
-            console.log("One IP:" , data_query.length)
+        if(data_query.length == 1){
+          newTimesPlayed = data_query[0].num_times_played+1
+          database.update({ ip: ip }, { $set: { num_times_played: newTimesPlayed} })
+          
         }
         //Else Create IP
         else{
-            console.log("Here")
+            if(data_query.length >1){
+              console.log("DB ERROR?")
+            }
+            data.num_times_played = 1
             database.insert(data);
         }
 
     })
     //response.json(data);
 });
+
